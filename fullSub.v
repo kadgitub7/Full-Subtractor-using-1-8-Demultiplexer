3861eb9d696feb11a017c9e1502e451b03a875e4
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-20 08:22:51 -0400

    need name for module

--- fullSub.v
+++ fullSub.v
@@ -10,7 +10,7 @@ module fullSub(
     );
     wire Y0, Y1, Y2, Y3, Y4, Y5, Y6, Y7;
     
-    eightOneDeMultiplexer(1'b1, S2,S1,S0,I,Y0,Y1,Y2,Y3,Y4,Y5,Y6,Y7);
+    eightOneDeMultiplexer demux1(1'b1, S2,S1,S0,I,Y0,Y1,Y2,Y3,Y4,Y5,Y6,Y7);
     
     assign D = Y1 | Y2 | Y4 | Y7;
     assign Bo = Y1 | Y2 | Y3 | Y7;
